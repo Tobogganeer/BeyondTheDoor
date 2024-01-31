f6b316885511cb72c356d5be77068ed5daa40b2b
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-01-30 23:20:40 -0500

    Import window enum generation toggles

--- Assets/Scripts/Dialogue System/Importer/Editor/DialogueImportWindow.cs
+++ Assets/Scripts/Dialogue System/Importer/Editor/DialogueImportWindow.cs
@@ -6,17 +6,23 @@ using UnityEditor;
 public class DialogueImportWindow : EditorWindow
 {
     TextAsset tsvFile;
+    bool generateCharactersEnum;
+    bool generateStatusEnum;
+    bool generateIDEnum = true;
 
     [MenuItem("Dialogue/Import Window")]
     public static void ShowWindow()
     {
         DialogueImportWindow window = EditorWindow.GetWindow<DialogueImportWindow>();
         window.titleContent = new GUIContent("Import Dialogue");
-        window.minSize = new Vector2(300, 600);
+        window.minSize = new Vector2(300, 400);
     }
 
     private void OnGUI()
     {
-        tsvFile = EditorGUILayout.ObjectField(tsvFile, typeof(TextAsset), false) as TextAsset;
+        tsvFile = EditorGUILayout.ObjectField("Lines File", tsvFile, typeof(TextAsset), false) as TextAsset;
+        generateCharactersEnum = EditorGUILayout.Toggle("Generate Characters Enum", generateCharactersEnum);
+        generateStatusEnum = EditorGUILayout.Toggle("Generate Status Enum", generateStatusEnum);
+        generateIDEnum = EditorGUILayout.Toggle("Generate LineID Enum", generateIDEnum);
     }
 }
