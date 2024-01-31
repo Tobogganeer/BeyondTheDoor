2e5a6c40fcf0f4381a10fa0b8ac4277229d1a139
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-01-30 23:58:55 -0500

    Set up CodeFile name and path

--- Assets/Scripts/Dialogue System/CodeGen/CodeFile.cs
+++ Assets/Scripts/Dialogue System/CodeGen/CodeFile.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
+using System.IO;
 
 namespace ToBOE.Dialogue.CodeGen
 {
     public class CodeFile
     {
-        public string Path { get; private set; }
+        public string FileName { get; private set; }
+        public string FolderPath { get; private set; }
+        public string FullPath { get; private set; }
+        
         public uint IndentLevel { get; private set; }
 
-
         private StringBuilder builder;
+
+        public CodeFile(string fileName, string relativeFolderPath)
+        {
+            FileName = fileName;
+            FolderPath = Path.Combine(Application.dataPath, relativeFolderPath);
+            FullPath = Path.Combine(FolderPath, FileName);
+
+            IndentLevel = 0;
+            builder = new StringBuilder();
+        }
     }
 }
