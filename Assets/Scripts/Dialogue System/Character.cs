e3d0742e47bd830b4fce13c49e7928b99a675584
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2024-02-19 16:08:05 -0500

    Added Character constructor

--- Assets/Scripts/Dialogue System/Character.cs
+++ Assets/Scripts/Dialogue System/Character.cs
@@ -11,8 +11,46 @@ namespace ToBOE.Dialogue
     [Serializable]
     public partial class Character
     {
+        // ============ Variables ============
         public CharacterID ID { get; private set; }
+        public string Name { get; set; }
         public CharacterStatus Status { get; set; }
+
+
+        // ============ Useful Properties ============
+        /// <summary>
+        /// Has this character been to the door?
+        /// </summary>
+        public bool Encountered => Status >= CharacterStatus.AtDoor;
+        /// <summary>
+        /// Has this character been inside the cabin?
+        /// </summary>
+        public bool Introduced => Status >= CharacterStatus.PartyMember;
+
+
+        // ============ Constants ============
+        static readonly string UnknownCharacterName = "(Unknown)";
+
+
+        public Character(CharacterID id)
+        {
+            ID = id;
+            Name = id.ToString(); // Can be changed later
+            Status = CharacterStatus.NotMet;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Gets the name of the character if they have been introduced.
+        /// </summary>
+        /// <returns></returns>
+        public string GetCurrentName()
+        {
+            return Introduced ? Name : UnknownCharacterName;
+        }
     }
 
     public enum CharacterStatus
@@ -21,7 +59,8 @@ namespace ToBOE.Dialogue
         AtDoor,
         LeftOutside,
         PartyMember,
-        Scavenging,
+        ScavengingDefenseless,
+        ScavengingWithShotgun,
         DeadWhileScavenging,
         KilledByBear,
         Kidnapped,
