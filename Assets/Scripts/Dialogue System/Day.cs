887c594d94a4e53d24cbc465441614e7151efa88
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2024-02-19 16:26:41 -0500

    Added stage to Day class

--- Assets/Scripts/Dialogue System/Day.cs
+++ Assets/Scripts/Dialogue System/Day.cs
@@ -5,6 +5,9 @@ using System;
 
 namespace ToBOE.Dialogue
 {
+    /// <summary>
+    /// Represents the course of a day.
+    /// </summary>
     [Serializable]
     public class Day
     {
@@ -13,5 +16,18 @@ namespace ToBOE.Dialogue
 
         public int Number => number;
         public CharacterID ArrivingCharacter => arrivingCharacter;
+        public Stage Stage { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a stage of the day.
+    /// </summary>
+    public enum Stage
+    {
+        SpeakingWithParty,
+        SendingScavengers,
+        FixingOvercrowding,
+        RadioLoreTime,
+        DealingWithArrival
     }
 }
