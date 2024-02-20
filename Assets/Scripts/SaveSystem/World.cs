326c93a47ed144f9a7e2adc6768b0ff1bd99537d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-19 23:43:11 -0500

    Made empty functions for world to serialize and deserialize game data

--- Assets/Scripts/SaveSystem/World.cs
+++ Assets/Scripts/SaveSystem/World.cs
@@ -13,12 +13,63 @@ namespace BeyondTheDoor.SaveSystem
 
         public void Serialize(ByteBuffer buf)
         {
-            throw new System.NotImplementedException();
+            SaveCabin(buf);
+            SaveDays(buf);
+            SaveCharacters(buf);
+            SaveLines(buf);
         }
 
         public void Deserialize(ByteBuffer buf)
         {
-            throw new System.NotImplementedException();
+            // Load everything in the same order
+            LoadCabin(buf);
+            LoadDays(buf);
+            LoadCharacters(buf);
+            LoadLines(buf);
         }
+
+        #region Saving
+        private void SaveCabin(ByteBuffer buf)
+        {
+
+        }
+
+        private void SaveDays(ByteBuffer buf)
+        {
+
+        }
+
+        private void SaveCharacters(ByteBuffer buf)
+        {
+
+        }
+
+        private void SaveLines(ByteBuffer buf)
+        {
+
+        }
+        #endregion
+
+        #region Loading
+        private void LoadCabin(ByteBuffer buf)
+        {
+
+        }
+
+        private void LoadDays(ByteBuffer buf)
+        {
+
+        }
+
+        private void LoadCharacters(ByteBuffer buf)
+        {
+
+        }
+
+        private void LoadLines(ByteBuffer buf)
+        {
+
+        }
+        #endregion
     }
 }
