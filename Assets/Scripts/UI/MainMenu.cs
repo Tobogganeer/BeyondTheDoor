3449c6f0d6272a26488bce7b47fba2227ae1f72d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-25 16:55:43 -0500

    Main Menu button functionality

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,30 +4,58 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject homePage;
+    public GameObject continueButton;
+    public TMPro.TMP_Text continueButtonText;
     public GameObject quitOverlay;
+
+    [Space]
+    public GameObject savesPage;
+
+    [Space]
+    public GameObject settingsPage; // TODO: Change type to be the settings page type
 
     private void Start()
     {
         quitOverlay.SetActive(false);
+
+        // TODO: Check if we can continue
+        continueButton.SetActive(false);
+
     }
 
     public void Continue()
     {
-
+        throw new System.NotImplementedException();
     }
 
     public void Play()
     {
-
+        homePage.SetActive(false);
+        savesPage.SetActive(true);
+        settingsPage.SetActive(false);
     }
 
     public void Settings()
     {
-
+        homePage.SetActive(false);
+        savesPage.SetActive(false);
+        settingsPage.SetActive(true);
     }
 
     public void Quit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 
+    public void ReturnToHome()
+    {
+        homePage.SetActive(true);
+        savesPage.SetActive(false);
+        settingsPage.SetActive(false);
     }
 }
