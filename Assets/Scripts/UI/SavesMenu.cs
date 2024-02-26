736f203788b1b586690ed19ba5439d80fd144e5c
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-25 20:13:39 -0500

    Code for selecting specific slots

--- Assets/Scripts/UI/SavesMenu.cs
+++ Assets/Scripts/UI/SavesMenu.cs
@@ -1,10 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SavesMenu : MonoBehaviour
 {
+    public int numSaves = 3;
+    public GameObject[] selectedSlotHighlights;
+
+    [Space]
+    public TMP_Text saveSlotText;
+
+    [Space]
+    public GameObject occupiedSlotInfo;
+    public TMP_Text saveDateText;
+    public TMP_Text dayText;
+
+    [Space]
+    public GameObject emptySlotInfo;
+
+    int currentSlot = -1;
+
+    private void Start()
+    {
+        SelectSlot(1); // Default slot
+    }
+
     public void SelectSlot(int slot)
+    {
+        for(int i = 0; i < selectedSlotHighlights.Length; i++)
+        {
+            selectedSlotHighlights[i].SetActive(i == slot);
+        }
+
+        //occupiedSlotInfo.SetActive(false);
+        //emptySlotInfo.SetActive(true);
+
+        saveSlotText.text = "Slot " + (slot + 1);
+    }
+
+    public void StartNewGame()
     {
 
     }
