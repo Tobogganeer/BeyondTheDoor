54f3c0b8864aac419366e679a2e73b7dbde418c7
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-25 19:59:03 -0500

    Saves menu GUI and empty functions complete

---
+++ Assets/Scripts/UI/SavesMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavesMenu : MonoBehaviour
+{
+    public void SelectSlot(int slot)
+    {
+
+    }
+
+    public void LoadCurrentSave()
+    {
+
+    }
+
+    public void DeleteCurrentSave()
+    {
+
+    }
+}
