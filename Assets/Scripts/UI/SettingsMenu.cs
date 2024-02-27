86c95e838b4bee5672dba4d48dbeab7d2a5bf732
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-27 18:24:06 -0500

    Linked up GUI to settings

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public MenuSlider masterVolumeSlider;
+    public MenuSlider sfxVolumeSlider;
+    public MenuSlider ambientVolumeSlider;
+    public MenuSlider musicVolumeSlider;
+    public MenuSlider dialogueVolumeSlider;
+    public MenuSlider textSpeedSlider;
+
     Canvas canvas;
 
     private void Awake()
     {
         canvas = GetComponent<Canvas>();
+
+        // Register callbacks to change the values in the settings
+        masterVolumeSlider.ValueChanged += (value) => Settings.MasterVolume = value;
+        sfxVolumeSlider.ValueChanged += (value) => Settings.SfxVolume = value;
+        ambientVolumeSlider.ValueChanged += (value) => Settings.AmbientVolume = value;
+        musicVolumeSlider.ValueChanged += (value) => Settings.MusicVolume = value;
+        dialogueVolumeSlider.ValueChanged += (value) => Settings.DialogueVolume = value;
+        textSpeedSlider.ValueChanged += (value) => Settings.TextSpeed = value;
     }
 
     public void Open()
     {
         canvas.enabled = true;
+
+        // Set the appropriate values of our sliders
+        masterVolumeSlider.value = Settings.MasterVolume;
+        sfxVolumeSlider.value = Settings.SfxVolume;
+        ambientVolumeSlider.value = Settings.AmbientVolume;
+        musicVolumeSlider.value = Settings.MusicVolume;
+        dialogueVolumeSlider.value = Settings.DialogueVolume;
+        textSpeedSlider.value = Settings.TextSpeed;
     }
 
     public void Close()
     {
         canvas.enabled = false;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Save our settings to disk
+        Settings.Save();
     }
 }
