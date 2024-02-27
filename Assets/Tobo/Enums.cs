b68cca443839256be3347715b1b1d8d1be5a264e
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-27 13:19:40 -0500

    Seperated game scenes into stages

--- Assets/Tobo/Enums.cs
+++ Assets/Tobo/Enums.cs
@@ -2,7 +2,32 @@
 public enum Level : byte
 {
     MainMenu,
-    Game,
+    /// <summary>
+    /// Talking with people, etc.
+    /// </summary>
+    Morning,
+    /// <summary>
+    /// Sending out scavengers.
+    /// </summary>
+    Noon,
+    /// <summary>
+    /// Kicking people out to fix overcrowding.
+    /// </summary>
+    Afternoon,
+    /// <summary>
+    /// Getting lore on the radio, leading into the door.
+    /// </summary>
+    Evening,
+    /// <summary>
+    /// Letting someone in or kicking them out.
+    /// </summary>
+    Door,
+    /// <summary>
+    /// The final epilogue about what you've done, etc.
+    /// </summary>
     BorderEnd,
+    /// <summary>
+    /// Getting eaten by the bear, starving, dying on the way to the border, etc.
+    /// </summary>
     EarlyEnd
 }
