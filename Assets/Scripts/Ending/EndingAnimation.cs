f33b526624844d1e57e6af696f3764de21d98021
dg015 <bruno.barbosa8@yahoo.com>
2024-04-10 17:59:01 -0400

    Dad ending done

--- Assets/Scripts/Ending/EndingAnimation.cs
+++ Assets/Scripts/Ending/EndingAnimation.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 using static UnityEditor.PlayerSettings;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.UIElements;
+using static Unity.Burst.Intrinsics.X86;
+using static UnityEngine.ParticleSystem;
+using static UnityEngine.Rendering.DebugUI.Table;
+using Unity.Burst.Intrinsics;
+using UnityEngine.UIElements.Experimental;
+using static UnityEngine.Rendering.DebugUI;
+using System.Net.NetworkInformation;
+using System;
 
 public class EndingAnimation : MonoBehaviour
 {
@@ -35,6 +43,18 @@ public class EndingAnimation : MonoBehaviour
         if (Character.Player.Status == CharacterStatus.AliveAtBorder && Character.Bob.Status == CharacterStatus.AliveAtBorder && Character.Jessica.Status == CharacterStatus.AliveAtBorder)
         {
             yield return WriteList(timeBetweenTransitions, "I thought Bob was going to break his promise of Jessica", "taking care of Jessica even after reaching the border”, “I was wrong”, “Very wrong” “They are together now,” “I guess that”,” being stuck near someone”, “you dislike for such a long time can change their views”, “ I guess it was worth it in the end" );
+        }
+        if (Character.Player.Status == CharacterStatus.AliveAtBorder && Character.Bob.Status == CharacterStatus.AliveAtBorder && Character.Jessica.Status == CharacterStatus.AliveAtBorder)
+        {
+            yield return WriteList(timeBetweenTransitions, "I thought Bob was going to break his promise of Jessica", "taking care of Jessica even after reaching the border", "I was wrong", "Very wrong", "They are together now", "I guess that”,” being stuck near someone", "you dislike for such a long time can change their views", " I guess it was worth it in the end");
+        }
+        if (Character.Player.Status == CharacterStatus.AliveAtBorder && Character.Bob.Status == CharacterStatus.AliveAtBorder && Character.Violet.Status == CharacterStatus.AliveAtBorder)
+        {
+            yield return WriteList(timeBetweenTransitions,"Violet opened a mechanic shop", "Violet promised Bob this job would turn his life around", "if his life didn’t change for the better then she would let him go to the army", "After some year I stopped there to see how they were going", "Strange, it doesn’t look like the same Bob I knew", "he wasn’t rude anymore", "he wasn’t polite either", "but I can see his making efforts", "It the first time I saw Bob smile", "I guess it was worth it in the end");
+        }
+        if (Character.Player.Status == CharacterStatus.AliveAtBorder && Character.Jessica.Status == CharacterStatus.AliveAtBorder && Character.Violet.Status == CharacterStatus.AliveAtBorder)
+        {
+            yield return WriteList(timeBetweenTransitions, "I was feeling very tired", "I felt my leg tremble as I walked", "I blinked and collapsed", "I was woken up being carried in a sledge by two people", "I head familiar voices", "As my vision started to go back to normal", "The shapes became defined", "It was mark and his kid ", "I thought I was done for", "He was happy that I woke up", " He said he found me lying in the ground unconscious" , " WIth the gun I gave him he was able to defend himself until he found his son", " we are approaching the border now", " I guess that are happy endings", "Even if they are few" );
         }
     }
     IEnumerator WriteList(float delay, params string[] textToDisplay)
