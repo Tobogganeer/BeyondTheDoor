41620b0e400c4c7ac4a23c7a58b816b3e74f4c42
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-04-10 23:33:17 -0400

    Created dday 6 init

--- Assets/Scripts/Initialization/Day6Init.cs
+++ Assets/Scripts/Initialization/Day6Init.cs
@@ -1,10 +1,11 @@
+using BeyondTheDoor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Day4Init1 : DayBehaviour
+public class Day2Init : DayBehaviour
 {
-    protected override int GetDay() => 4;
+    protected override int GetDay() => 6;
 
     protected override void Initialize()
     {
