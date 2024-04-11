1e3070c81e90428c35f00b45eeee1754e10b1b6e
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-04-11 04:38:54 -0400

    Fixed missing door links

--- Assets/Scripts/DayCounterUI.cs
+++ Assets/Scripts/DayCounterUI.cs
@@ -7,21 +7,8 @@ using UnityEngine;
 public class DayCounterUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI dayCounter;
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    //dasd
-
-    private void getDay()
-    {
-        dayCounter.text = "Day:" + Day.DayNumber.ToString() + "\n" + Day.Stage.ToString() ;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        getDay();
+        dayCounter.text = "Day " + Day.DayNumber.ToString() + " - " + Day.Stage.ToTimeString();
     }
 }
